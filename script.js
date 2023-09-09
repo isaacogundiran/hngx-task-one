d9839a0eba24ba4fae763f65e1014acb86e8a409
isaacogundiran <itsmeizic@gmail.com>
2023-09-09 19:49:35 +0100

    correction

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const slackUsername = "Isaacogundiran";
+const slackNameElement = document.querySelector('[data-testid="slackUsername"]').innerHTML = slackUsername;
+
+
+
+
+const slackProfilePictureUrl = "https://ca.slack-edge.com/T05FFAA91JP-U05S0KL1K2L-b4ce560f1a66-48";
+const slackDisplayImageElement = document.querySelector('[data-testid="slackDisplayImage"]')
+
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const today = new Date();
+const currentDayIndex = currentDate.getDay();
+const currentDayOfTheWeek = daysOfWeek[currentDayIndex];
+const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+
+
+
+const currentUTCTimeMillis = Date.now();
+const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+
+
+
+const myTrack = "Frontend";
+const myTrackElement = document.querySelector('[data-testid="myTrack"]');
+
+
+
+
+const githubRepoURL = "https://github.com/isaacogundiran/hngx-task-one.git";
+const githubURLElement = document.querySelector('[data-testid="githubURL"]');
+
+
+
+alert("my code")
+
