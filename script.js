84d0bdc396fa0701c6f16ccdf7554bd95150e208
isaacogundiran <itsmeizic@gmail.com>
2023-09-11 13:27:52 +0100

    edited

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
+// JavaScript code to fetch and display current day of the week
+const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDay = new Date().getDay();
+  document.getElementById(
+    "currentDay"
+  ).textContent = `Today is: ${daysOfWeek[currentDay]}`;
 
-const slackUsername = "Isaacogundiran";
-const slackNameElement = document.querySelector('[data-testid="slackUsername"]').innerHTML = slackUsername;
-
-
-
-
-const slackProfilePictureUrl = "https://ca.slack-edge.com/T05FFAA91JP-U05S0KL1K2L-b4ce560f1a66-48";
-const slackDisplayImageElement = document.querySelector('[data-testid="slackDisplayImage"]')
-
-
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const today = new Date();
-const currentDayIndex = currentDate.getDay();
-const currentDayOfTheWeek = daysOfWeek[currentDayIndex];
-const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-
-
-
-const currentUTCTimeMillis = Date.now();
-const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-
-
-
-const myTrack = "Frontend";
-const myTrackElement = document.querySelector('[data-testid="myTrack"]');
-
-
-
-
-const githubRepoURL = "https://github.com/isaacogundiran/hngx-task-one.git";
-const githubURLElement = document.querySelector('[data-testid="githubURL"]');
+  // JavaScript code to fetch and display current UTC time
+  fetch("https://worldtimeapi.org/api/ip")
+    .then((response) => response.json())
+    .then((data) => {
+      const utcTime = new Date(data.utc_datetime).toUTCString();
+      document.getElementById(
+        "currentUTCTime"
+      ).textContent = `Current UTC Time: ${utcTime}`;
+    })
+    .catch((error) => {
+      console.error("Error fetching UTC time:", error);
+    });
 
 
 
-alert("my code")
 
