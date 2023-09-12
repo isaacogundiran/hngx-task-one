51d8285d28ecb8ffa4ce7bba892a56dc14d16dd2
isaacogundiran <itsmeizic@gmail.com>
2023-09-12 10:47:38 +0100

    final edit

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-// JavaScript code to fetch and display current day of the week
-const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDay = new Date().getDay();
-  document.getElementById(
-    "currentDay"
-  ).textContent = `Today is: ${daysOfWeek[currentDay]}`;
 
-  // JavaScript code to fetch and display current UTC time
-  fetch("https://worldtimeapi.org/api/ip")
-    .then((response) => response.json())
-    .then((data) => {
-      const utcTimeMilliseconds = new Date(data.utc_datetime).getTime();
-      document.getElementById(
-        "currentUTCTime"
-      ).textContent = `Current UTC Time: ${utcTimeMilliseconds}`;
-    })
-    .catch((error) => {
-      console.error("Error fetching UTC time:", error);
-    });
+    
+      function addZero(x, n) {
+        while (x.toString().length < n) {
+          x = "0" + x;
+        }
+        return x;
+      }
 
+      const d = new Date();
+      let h = addZero(d.getUTCHours(), 2);
+      let m = addZero(d.getUTCMinutes(), 2);
+      let s = addZero(d.getUTCSeconds(), 2);
+      let ms = addZero(d.getUTCMilliseconds(), 3);
+      let time = h + ":" + m + ":" + s + ":" + ms;
+      document.getElementById("currentUTCTime").innerHTML = time;
+    
 
-
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day = weekday[d.getUTCDay()];
+document.getElementById("currentDay").innerHTML = day;
 
