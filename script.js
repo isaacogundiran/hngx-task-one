7f2073f6b435726570cbd8f7bb5abdc646aebda0
Ogundiran Isaac <104150943+isaacogundiran@users.noreply.github.com>
2023-09-12 12:27:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@
  const enigmaticWeekday = x.getDay(); 
   
  const mysticDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]'); 
- mysticDayElement.textContent = `Currnt Day of The Week: ${weekdays[enigmaticWeekday]}`; 
+ mysticDayElement.textContent = `Current Day of The Week: ${weekdays[enigmaticWeekday]}`; 
   
  const mysticalUTCTime = x.getTime(); 
   
  const arcaneTimeElement = document.querySelector('[data-testid="currentUTCTime"]'); 
- arcaneTimeElement.textContent = `Current UTC Time: ${mysticalUTCTime} ms`; 
+ arcaneTimeElement.textContent = `Current UTC Time: ${mysticalUTCTime}`; 
   
     
 
